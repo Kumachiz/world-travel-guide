40d270e67ef7e65377cc10188985ccd5ce0a49a9
Michael Munyaneza <kuamchiz@gmail.com>
2021-11-14 02:48:23 -0800

    remove unused ui js code

--- js/scripts.js
+++ js/scripts.js
@@ -31,63 +31,7 @@ $(document).ready(function() {
     // }
     // else if (continent === 'America' && "weather" === 'Dry' || 'Humid' && "type" === 'Fun' && "price" === 'Expensive' && "transportation" === 'Plane' || 'Car'){
     //   $("#America_text").show();
-  event.preventDefault(); 
-  
+  event.preventDefault();
+    
 });
 });
-
-const toggleColourModeBtn = document.getElementById("colourModeBtn");
-const currentColourMode = localStorage.getItem("colourMode");
-const sysIsDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-getSavedColourMode(currentColourMode);
-getColourModeBtnText(currentColourMode, sysIsDark);
-
-toggleColourModeBtn.addEventListener("click", function () {
-    toggleColourMode(sysIsDark);
-    setAndSaveColourMode();
-});
-
-function getSavedColourMode(savedMode) {
-    if (localStorage.getItem("overRideSysColour") == "true") {
-        document.body.classList.remove('systemDarkPreference');
-        document.body.classList.toggle(savedMode);
-    }
-}
-
-function getColourModeBtnText(mode, currentSysIsDark) {
-    if((mode == "dark") || (currentSysIsDark && mode != "light")){
-        assignColourModeBtnTextLight();
-    } else {
-        assignColourModeBtnTextDark();
-    }
-}
-
-function toggleColourMode(currentSysIsDark) {
-    const hasSystemDarkClass = document.body.classList.contains('systemDarkPreference');
-    const isDark = (hasSystemDarkClass && currentSysIsDark) || document.body.classList.contains('dark');
-
-    document.body.classList.remove('systemDarkPreference');
-    document.body.classList.toggle('dark', !isDark);
-}
-
-function setAndSaveColourMode() {
-    let colourMode;
-    if (document.body.classList.contains("dark")) {
-        colourMode = "dark";
-        assignColourModeBtnTextLight();
-    } else {
-        colourMode = "light";
-        assignColourModeBtnTextDark();
-    }
-    localStorage.setItem("colourMode", colourMode);
-    localStorage.setItem("overRideSysColour", "true")
-}
-
-function assignColourModeBtnTextLight() {
-    toggleColourModeBtn.innerText = "Go Light 🌞"
-}
-
-function assignColourModeBtnTextDark() {
-    toggleColourModeBtn.innerText = "Go Dark 🌚"
-}
